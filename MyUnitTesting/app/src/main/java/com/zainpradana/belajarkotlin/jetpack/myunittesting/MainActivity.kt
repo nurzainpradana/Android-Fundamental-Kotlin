22da390e17989ca3d6f2b0f1c211a9646782f042
nurzainpradana <pradananurzain@gmail.com>
2021-04-28 22:01:02 +0700

    add library mockito

--- MyUnitTesting/app/src/main/java/com/zainpradana/belajarkotlin/jetpack/myunittesting/MainActivity.kt
+++ MyUnitTesting/app/src/main/java/com/zainpradana/belajarkotlin/jetpack/myunittesting/MainActivity.kt
@@ -2,10 +2,88 @@ package com.zainpradana.belajarkotlin.jetpack.myunittesting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import com.zainpradana.belajarkotlin.jetpack.myunittesting.databinding.ActivityMainBinding
+import com.zainpradana.belajarkotlin.jetpack.myunittesting.model.CuboidModel
+import com.zainpradana.belajarkotlin.jetpack.myunittesting.viewmodel.MainViewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+    private lateinit var mainViewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        mainViewModel = MainViewModel(CuboidModel())
+
+        activityMainBinding.btnSave.setOnClickListener(this)
+        activityMainBinding.btnCalculateCircumference.setOnClickListener(this)
+        activityMainBinding.btnCalculateSurfaceArea.setOnClickListener(this)
+        activityMainBinding.btnCalculateVolume.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        val length = activityMainBinding.edtLength.text.toString().trim()
+        val width = activityMainBinding.edtWidth.text.toString().trim()
+        val height = activityMainBinding.edtHeight.text.toString().trim()
+
+        when {
+            TextUtils.isEmpty(length) -> {
+                activityMainBinding.edtLength.error = "Field ini tidak boleh kosong"
+            }
+            TextUtils.isEmpty(width) -> {
+                activityMainBinding.edtWidth.error = "Field ini tidak boleh kosong"
+            }
+            TextUtils.isEmpty(height) -> {
+                activityMainBinding.edtHeight.error = "Field ini tidak boleh kosong"
+            }
+
+            else -> {
+                val valueLength = length.toDouble()
+                val valueWidth = width.toDouble()
+                val valueHeight = height.toDouble()
+
+                when (v.id) {
+                    R.id.btn_save -> {
+                        mainViewModel.save(valueLength, valueWidth, valueHeight)
+                        visible()
+                    }
+
+                    R.id.btn_calculate_circumference -> {
+                        activityMainBinding.tvResult.text = mainViewModel.getCircumference().toString()
+                        gone()
+                    }
+
+                    R.id.btn_calculate_surface_area -> {
+                        activityMainBinding.tvResult.text = mainViewModel.getSurfaceArea().toString()
+                        gone()
+                    }
+
+                    R.id.btn_calculate_volume -> {
+                        activityMainBinding.tvResult.text = mainViewModel.getVolume().toString()
+                        gone()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun visible() {
+        activityMainBinding.btnCalculateVolume.visibility = View.VISIBLE
+        activityMainBinding.btnCalculateCircumference.visibility = View.VISIBLE
+        activityMainBinding.btnCalculateSurfaceArea.visibility = View.VISIBLE
+        activityMainBinding.btnSave.visibility = View.GONE
+    }
+
+    private fun gone() {
+        activityMainBinding.btnCalculateVolume.visibility = View.GONE
+        activityMainBinding.btnCalculateCircumference.visibility = View.GONE
+        activityMainBinding.btnCalculateSurfaceArea.visibility = View.GONE
+        activityMainBinding.btnSave.visibility = View.VISIBLE
     }
 }
